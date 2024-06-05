7ea53d1cb2104e04faad6f755fe39e49b94840bc
DESKTOP-MBN6FJJ\Admin <nguyenloi.itse@gmail.com>
2024-06-05 18:46:04 +0700

    Testimonal Controller CRUD operations and cshtml setup

---
+++ Daisin/Areas/Admin/Controllers/TestimonalController.cs
@@ -0,0 +1,56 @@
+﻿using EntityLayer.WebApplication.ViewModels.TestimonalVM;
+using Microsoft.AspNetCore.Mvc;
+using ServiceLayer.Services.Abstract;
+
+namespace Daisin.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/Testimonal")]
+    public class TestimonalController : Controller
+    {
+        private readonly ITestimonalService _testimonalService;
+
+        public TestimonalController(ITestimonalService testimonalService)
+        {
+            _testimonalService = testimonalService;
+        }
+        [HttpGet("GetTestimonalList")]
+        public async Task<IActionResult> GetTestimonalList()
+        {
+            var testimonalList = await _testimonalService.GetAllAsync();
+            return View(testimonalList);
+        }
+
+        [HttpGet("AddTestimonal")]
+        public IActionResult AddTestimonal()
+        {
+            return View();
+        }
+        [HttpPost("AddTestimonal")]
+        public async Task<IActionResult> AddTestimonal(TestimonalAddVM request)
+        {
+            await _testimonalService.AddTestimonalAsync(request);
+            return RedirectToAction("GetTestimonalList", "Testimonal", new { Areas = ("Admin") });
+        }
+
+        [HttpGet("UpdateTestimonal")]
+        public async Task<IActionResult> UpdateTestimonal(int id)
+        {
+            var testimonal = await _testimonalService.GetTestimonalById(id);
+            return View(testimonal);
+        }
+        [HttpPost("UpdateTestimonal")]
+        public async Task<IActionResult> UpdateTestimonal(TestimonalUpdateVM request)
+        {
+            await _testimonalService.UpdateTestimonalAsync(request);
+            return RedirectToAction("GetTestimonalList", "Testimonal", new { Areas = ("Admin") });
+        }
+
+        [HttpGet("Delete/{id}")]
+        public async Task<IActionResult> DeleteTestimonal(int Id)
+        {
+            await _testimonalService.DeleteTestimonalAsync(Id);
+            return RedirectToAction("GetTestimonalList", "Testimonal", new { Areas = ("Admin") });
+        }
+    }
+}
--- EntityLayer/WebApplication/ViewModels/TestimonalVM/TestimonalAddVM.cs
+++ EntityLayer/WebApplication/ViewModels/TestimonalVM/TestimonalAddVM.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,5 +14,6 @@ namespace EntityLayer.WebApplication.ViewModels.TestimonalVM
 		public string Title { get; set; } = null!;
 		public string FileName { get; set; } = null!;
 		public string FileType { get; set; } = null!;
+		public IFormFile Photo { get; set; } = null!;
 	}
 }
